ee8326eb6514ca32ae253cab6bc62a1bfe443ae2
TheGuy191919 <evan_1998@sina.com>
2014-07-24 15:31:20 -0400

    How Do I use GIT??!!!??

---
+++ src/Team5163/TeamTrackerServer/Server.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Team5163.TeamTrackerServer;
+
+/**
+ *
+ * @author Yiwen Dong
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
